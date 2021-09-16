10f278c3f61866d5b4171098e0648b3ab2f87d78
vasilesirbu1957 <vasilesirbu1957@gmail.com>
2021-09-16 19:10:30 +0300

    Update scenes.js

--- js/scenes.js
+++ js/scenes.js
@@ -1,4 +1,4 @@
-export {createSkyboxFlowersScene, createHemisphereLight}; 
+export {createSkyboxFlowersScene, createHemisphereLight};
 
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
@@ -21,9 +21,17 @@ function createSkyboxFlowersScene() {
   return skyboxFlowersScene;
 }
 
-//hemisphereLight
+//hemisphere light to have light from everywhere
 function createHemisphereLight({skyColor,groundColor,intensity},scene){
   const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
   scene.add(light); //add this light to the scene
+  return light;
+}
+
+//directional light to have light from the given direction
+function createDirectionalLight({color, intensity, x,y,z}, cam_or_scene){
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(x,y,z);
+  cam_or_scene.add(light); //add this light to the scene
   return light;
 }
