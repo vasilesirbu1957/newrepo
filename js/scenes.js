3d4dfa3fcedd7f4f8b12b42f10092245bce23595
vasilesirbu1957 <vasilesirbu1957@gmail.com>
2021-09-16 17:46:38 +0300

    Update scenes.js

--- js/scenes.js
+++ js/scenes.js
@@ -1,4 +1,4 @@
-export {createSkyboxFlowersScene};
+export {createSkyboxFlowersScene, createHemisphereLight};
 
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
@@ -19,4 +19,11 @@ function createSkyboxFlowersScene() {
   ]);
   skyboxFlowersScene.background = texture; //set background texture (cube texture)
   return skyboxFlowersScene;
-} 
+}
+
+//hemisphereLight
+function createHemisphereLight({skyColor,groundColor,intensity},scene){
+  const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+  scene.add(light); //add this light to the scene
+  return light;
+}
