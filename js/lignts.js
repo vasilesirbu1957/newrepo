fb39d26143b88600df6a077e7682cb51437c2242
vasilesirbu1957 <vasilesirbu1957@gmail.com>
2021-09-16 17:14:39 +0300

    Update lignts.js

--- js/lignts.js
+++ js/lignts.js
@@ -1,8 +1,9 @@
 export {createHemisphereLight};
 
-
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
 
+//hemisphereLight
 function createHemisphereLight({skyColor,groundColor,intensity},scene){
   const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
   scene.add(light); //add this light to the scene
